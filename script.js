bbb5805825909100ab1de79bb0363cdc904d3c22
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-08-17 10:32:50 +0300

    feat: add full functionality

--- script.js
+++ script.js
@@ -2,35 +2,32 @@ let gallery = document.querySelector('.gallery');
 let search = document.querySelector('.search__button');
 let input = document.querySelector('.form-control');
 let spinner = document.querySelector('.spinner-border');
-let morePicturesButton = document.querySelector('.btn');
-let url = `https://api.unsplash.com/search/photos?query=design&page=1&per_page=40&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
+let morePicturesButton = document.querySelector('.fa-chevron-circle-down');
+let url = `https://api.unsplash.com/search/photos?query=design&page=4&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
 let pageCounter = 0;
-let inputValue;
+let inputValue = 'design';
+
 getImageUrl();
 
 search.addEventListener('click', () => {
     gallery.innerHTML = '';
     inputValue = input.value;
-    url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=1&per_page=9&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
+    url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=1&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
     getImageUrl(url);
 });
 
 morePicturesButton.addEventListener('click', () => {
     pageCounter++;
-    url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=${pageCounter}&per_page=9&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
+    url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=${pageCounter}&client_id=dqSzPLB8JLA05l2Jp8CLC8CYn0w4FalflixxL7hosZc`;
     getImageUrl(url);
 });
 
 async function getImageUrl(requestedUrl = url) {
-    spinner.style.display = 'block';
     const res = await fetch(requestedUrl);
     const data = await res.json();
-    setTimeout(() => {
-        spinner.style.display = 'none';
-        return data.results.map(obj => createImg(obj.urls.regular));
-    }, 500)
+    data.results.length = 9;
+    return data.results.map(obj => createImg(obj.urls.regular));
 }
-
 
 function createImg(imgUrl) {
     let imgParent = document.createElement('div');
@@ -41,6 +38,11 @@ function createImg(imgUrl) {
     imgChild.style.backgroundPosition = 'center';
     imgChild.style.backgroundSize = 'cover';
     imgChild.setAttribute('href', imgUrl);
+    openImage(imgChild, imgUrl);
     imgParent.append(imgChild);
     gallery.append(imgParent);
+}
+
+function openImage(img, src) {
+    img.onclick = () => window.open(src, src);
 }
